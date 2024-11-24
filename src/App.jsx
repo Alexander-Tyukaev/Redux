790f83e2d3c0af23183914f3373b90c7a239ca70
Alexander-Tyukaev <Sanek4100@gmail.com>
2024-11-24 16:24:39 +0300

    none filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { addTaskAction, removeTaskAction } from "./store/taskReducer";
+import "../src/store/style.css"
 
 function App() {
 
@@ -25,14 +26,17 @@ const deleteTask = (id) =>{
 
   return (
     <div className="App">
-<input value={inputId} type="text"  onChange={(e) => setInputId(e.target.value)}/>
-<input value={inputName} type="text"  onChange={(e) => setInputName(e.target.value)}/>
-<button onClick={addTask}>Добавить задачу</button>
+    <div className="menu">
+<input value={inputId} type="text"  className="text_id" maxlength="3" onChange={(e) => setInputId(e.target.value)}/>
+<input value={inputName} type="text" className="text_name"  maxlength="20" onChange={(e) => setInputName(e.target.value)}/>
+<button onClick={addTask}  className="button1">Добавить задачу</button>
+</div>
 <div className="tasks">
   {tasks.map(task =>
-    <div key={task.id}>
-     id : {task.id} , name:{task.name}
-     <button onClick={() =>deleteTask(task.id)}>Удалить</button>
+    <div key={task.id} className="stroka">
+      <span className="task-id">{task.id}</span>
+      <span className="task-name">{task.name}</span>
+     <button className="button2" onClick={() =>deleteTask(task.id)}>Удалить</button>
     </div>
   )}
 </div>
